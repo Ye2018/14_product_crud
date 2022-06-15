ccd904bb45766361409e39531ddfa06d69219bd0
z349j <z349j@unb.ca>
2022-06-15 09:15:57 -0300

    Explain the purpose of the organization style

--- 03_good/helpers/UtilHelper.php
+++ 03_good/helpers/UtilHelper.php
@@ -1,5 +1,7 @@
 <?php
-
+// This file contains only one function, which is randomString. This function is inherited from 
+// previous version. The reason we put the function in a separate folder/file is we want to 
+// organize our codes in the OOP style.
 namespace app\helpers;
 
 class UtilHelper{
